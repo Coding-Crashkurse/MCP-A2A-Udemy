00c8cf23683264254fada9d80c294033f88dc9bc
Markus <datamastery87@gmail.com>
2025-09-11 10:17:24 +0200

    add code

--- 17_Middleware/client.py
+++ 17_Middleware/client.py
@@ -3,7 +3,7 @@ from fastmcp import Client
 from fastmcp.client.transports import StreamableHttpTransport
 
 async def main():
-    client = Client(StreamableHttpTransport("http://127.0.0.1:8017/mcp/"))
+    client = Client(StreamableHttpTransport("http://127.0.0.1:8080/mcp/"))
     async with client:
         res = await client.call_tool("add", {"a": 7, "b": 5})
         print("Result:", res.data)
--- 17_Middleware/server.py
+++ 17_Middleware/server.py
@@ -19,4 +19,4 @@ def add(a: int, b: int) -> int:
     return a + b
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="127.0.0.1", port=8017, path="/mcp/")
+    mcp.run(transport="http", host="127.0.0.1")
